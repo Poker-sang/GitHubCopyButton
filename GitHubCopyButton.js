76b699dc992bbc6fd1a9f27a95248b3e32cb7d10
Poker <poker_sang@outlook.com>
2025-12-16 11:53:26 +0800

    Update GitHubCopyButton.js

--- GitHubCopyButton.js
+++ GitHubCopyButton.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github copy clone command button
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Add a 1-click copy button with the "git clone --recurse-submodules ..." command, after the Code button on the GitHub repository page
 // @author       Poker powered by Copilot
 // @match        https://github.com/*
